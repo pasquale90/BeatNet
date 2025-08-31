4b937b0af4bfb8286bd7d324adff182f478d8d0c
Paschalis Melissas <melissaspaschalis@gmail.com>
2025-08-31 13:24:06 +0300

    add softmax in the exported model

--- onnx/exportModel.py
+++ onnx/exportModel.py
@@ -1,17 +1,28 @@
 import sys
 import os
-sys.path.insert(0, os.path.abspath("../src"))
 import torch
-from BeatNet.BeatNet import BeatNet
+import torch.nn as nn
 import onnx
+sys.path.insert(0, os.path.abspath("../src"))
+from BeatNet.BeatNet import BeatNet
 
+class BeatNetWithSoftmax(nn.Module):
+    def __init__(self, base_model):
+        super().__init__()
+        self.base_model = base_model
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, x):
+        logits = self.base_model(x)
+        return self.softmax(logits)
+    
 model_path = "beatnet_bda.onnx"
 
 # Initialize BeatNet
 estimator = BeatNet(1, mode='stream', inference_model='PF', plot=[], thread=False)
 
-# Get the PyTorch model (BDA)
-model = estimator.model
+# Get the PyTorch model (BDA) + softmax
+model = BeatNetWithSoftmax(estimator.model)
 
 # set to eval mode
 model.eval()
@@ -21,7 +32,7 @@ device = torch.device("cpu")
 model.to(device)
 
 # Create dummy input (batch_size, time_steps, feature_dim)
-print("Expected dim_in:", model.dim_in)
+print("Expected dim_in:", model.base_model.dim_in)
 dummy_input = torch.randn(1, 1, 272).to(device)
 
 # export to ONNX
