4af2d0e0c61cd523fc36fc2e014e3d79335f1ce1
Benoit Mascaro <benoit.mascaro@gmail.com>
2026-03-03 12:59:36 +0100

    cleanup testCPP

--- onnx/test/testCPP.cpp
+++ onnx/test/testCPP.cpp
@@ -5,9 +5,9 @@
 #include "BeatNet.h"
 #include <utility>
 
-//static int samplerate = 44100;
-static int buffersize = 2293; // at 22050 samples/sec
+static int buffersize = 2293; // in BeatNet at 22050 samples/sec
 typedef float bitQuantization; 
+
 namespace fs = std::filesystem;
 
 const fs::path currentFilePath = fs::path(__FILE__).parent_path();
@@ -21,7 +21,6 @@ int main() {
 	// iterate over files
 	for (const auto& audioSample : fs::directory_iterator(samplesPath))
 	{
-
 		std::string audiopath = audioSample.path().string();
 		std::cout << "Processing " << audiopath << std::endl;
 
@@ -32,9 +31,10 @@ int main() {
 
 		// load file
 		AudioFile<bitQuantization> audioFile;
-		audioFile.load(audiopath);
+		audioFile.load(audiopath);		
 
 		// values of parameters at the samplerate of the loaded file
+		const int numSamples = audioFile.getNumSamplesPerChannel();
 		const float sr_inputWavfile = static_cast<float>(audioFile.getSampleRate());
 		const int sr_Ratio = static_cast<int>(sr_inputWavfile / SR_BEATNET); // 44100 / 22050 = 2 
 		const float dt = 1.0f / sr_inputWavfile;
@@ -47,126 +47,48 @@ int main() {
 		BeatNet model;
 		model.setup(sr_inputWavfile, buffersize_current);
 
-		int numSamples = audioFile.getNumSamplesPerChannel();
-		
-		std::vector<float> time_beats;
-		std::vector<float> output0; 
-		std::vector<float> output1;
+		// predict beats and downbeats
+		std::vector<std::pair<float, int>> beatPositions;
+		std::vector<std::pair<float, int>> downBeatPositions;		
 
-		std::vector<std::pair<float, float>> beatPositions;
-		std::vector<std::pair<float, float>> downBeatPositions;		
-
-		for (int idx=0; idx + (buffersize_current) < numSamples; idx += (HOP_SIZE_current))
+		for (int idx=0; idx + buffersize_current < numSamples; idx += HOP_SIZE_current)
 		{
-			// predict beats
-			std::vector<float> audioInput(audioFile.samples[0].begin() + idx,
-										  audioFile.samples[0].begin() + idx + (buffersize_current));
+			std::vector<float> audioBlockInput(audioFile.samples[0].begin() + idx,
+										  audioFile.samples[0].begin() + idx + buffersize_current);
 			std::vector<float> output;
 			
-			if (model.process(audioInput, output))
+			if (model.process(audioBlockInput, output))
 			{
 				// output[0] : downbeat
 				// output[1] : beat
 				// output[2] : no beat
-				output0.push_back(output[0]);
-				output1.push_back(output[1]);
-
-				int indexMaxProbability = std::max_element(output.begin(), output.end()) - output.begin();
 
 				float time_index = (float)idx + (-(FRAME_LENGTH_current / 2) + (3 * HOP_SIZE_current));
+				float time_seconds = time_index * dt;
+
+				int indexMaxProbability = std::max_element(output.begin(), output.end()) - output.begin();
 				switch (indexMaxProbability)
 				{
 					case 0: // downbeat
-						time_beats.push_back(time_index);
-						downBeatPositions.push_back({ time_index * dt, 0.3f });
+						downBeatPositions.push_back({ time_seconds, 0 }); //
 						break;
 					case 1: // beat
-						time_beats.push_back(time_index);
-						beatPositions.push_back({ time_index * dt, 0.31f }); 
+						beatPositions.push_back({ time_seconds, 1 });
 						break;
 				}
-
 			}
 		}
 
-		// write beat positions to file, one per line
+		// write beat time positions to file, one per line
 		if (beatPositions.size() > 0)
 		{			
 			std::ofstream outFile(outputFilePath);
-			for (const auto& beat : beatPositions)
+			for (const auto& [time, beatValue] : beatPositions)
 			{
-				outFile << beat.first << "," << 1 << std::endl;
+				outFile << time << "," << beatValue << std::endl;
 			}
 			outFile.close();
 		}
-
-
-		if (!time_beats.empty())
-		{
-			std::vector<float> difference(time_beats.size() - 1);
-			std::vector<float> bpm(time_beats.size() - 1);
-
-			std::transform(time_beats.begin() + 1, time_beats.end(),
-				time_beats.begin(), difference.begin(), std::minus());
-
-			for (int i = 0; i < difference.size(); ++i)
-			{
-				bpm[i] = 60.0f / difference[i];
-			}
-
-			// std::cout << std::endl;
-			// for (auto& val : time_beats)
-			// {
-			// 	std::cout << val << std::endl;
-			// }
-			// std::cout << std::endl;
-		}
-		auto signal = audioFile.samples[0];
-		std::vector<float> times_plot(signal.size(), std::nan("1"));
-
-		for (auto& i : time_beats)
-		{
-			times_plot[(int) i] = 1.5f;
-		}
-		//signal.erase(signal.begin() + signal.size() /4, signal.end());
-		//times_plot.erase(times_plot.begin() + times_plot.size() / 4, times_plot.end());
-
-		std::vector<std::pair<float, float>> plot_output0(output0.size());
-		std::vector<std::pair<float, float>> plot_output1(output0.size());
-
-		float t0 = 0.0f;
-		for (float i = 0.0f; i < plot_output0.size(); ++i)
-		{
-			auto time = t0 + ((FRAME_LENGTH_current) + i * (HOP_SIZE_current)) * dt;
-			plot_output0[i] = { time , output0[i] };
-			plot_output1[i] = { time , output1[i] };
-		}
-
-
-		std::vector<float> time_vec(signal.size());
-		{
-			float index = 0.0f;
-			std::generate(time_vec.begin(), time_vec.end(), [t0, dt, &index]() {return t0 + (index++ * dt); });
-		}
-		{
-			int index = 0;
-			auto end = std::remove_if(signal.begin(), signal.end(), [&index](float x) {if (index > 9) index = 0; return index++ > 0; });
-			signal.erase(end, signal.end());
-		}
-
-		{
-			int index = 0;
-			auto end = std::remove_if(time_vec.begin(), time_vec.end(), [&index](float x) {if (index > 9) index = 0; return index++ > 0; });
-			time_vec.erase(end, time_vec.end());
-		}
-
-		std::vector<std::pair<float, float>> plot_signal(time_vec.size());
-		for (float i = 0.0f; i < time_vec.size(); ++i)
-		{
-			plot_signal[i] = { time_vec[i] , signal[i] };
-		}
-
-		int stop = 0;
 
 		std::cout << "Finished processing " << audiopath << " (" << numSamples << " samples)" << std::endl;
 	}
